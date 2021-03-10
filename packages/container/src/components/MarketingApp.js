a80973e13a37e603c116fd0745abb65e6b31a527
susana.martins <susana.martins@pt-320300-mbp16.lan>
2021-03-10 12:09:14 +0000

    initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,16 @@
+import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    }, []);
+
+    return (
+        <div ref={ref}>
+
+        </div>
+    )
+}
